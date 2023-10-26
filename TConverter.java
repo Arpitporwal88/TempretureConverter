141c74b94803bf6211569a4a31ea0d662310fe2b
Arpit Porwal <147469698+Arpitporwal88@users.noreply.github.com>
2023-10-26 20:50:09 +0530

    Update TConverter.java

--- TConverter.java
+++ TConverter.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 public class TConverter extends WindowAdapter implements ActionListener   {
     Frame f;
     Label l0, l1, l2, l3, l4;
-    Choice tc1, tc2;
+    Choice c1, c2;
     TextField t1, t2;
     Button b;
      public TConverter(){
@@ -15,23 +15,23 @@ public class TConverter extends WindowAdapter implements ActionListener   {
         l2 = new Label("Enter the Value:");
         l3 = new Label("Converted Value:");
         l4 = new Label("From");
-        tc1 = new  Choice ();
-         tc1.add("Celsius");    
-        tc1.add("Fahrenheit");     
-        tc2 = new  Choice ();
-         tc2.add("Celsius");    
-        tc2.add("Fahrenheit");    
+        c1 = new  Choice ();
+             c1.add("Celsius");    
+             c1.add("Fahrenheit");     
+        c2 = new  Choice ();
+             c2.add("Celsius");    
+             c2.add("Fahrenheit");    
         t1 = new TextField();
         t2 = new TextField();
         b = new Button("Convert");
         l0.setBounds(75, 80, 230, 20);
-        tc1.setBounds(200, 145, 100, 20);
+        c1.setBounds(200, 145, 100, 20);
         t1.setBounds(65, 145, 100, 20);
         l1.setBounds(200, 180, 50, 20);
         l4.setBounds(200, 125, 50, 20);
         l2.setBounds(65, 125, 100, 20);
         l3.setBounds(63, 180, 100, 20);
-        tc2.setBounds(200, 200, 100, 20);
+        c2.setBounds(200, 200, 100, 20);
         t2.setBounds(65, 200, 100, 20);
         b.setBounds(130, 250, 100, 20);
         b.setBackground(Color.LIGHT_GRAY);
@@ -44,8 +44,8 @@ public class TConverter extends WindowAdapter implements ActionListener   {
         b.setFont(new Font("Arial", Font.BOLD,12));
         b.addActionListener(this);
        f.add(l0);
-       f.add(tc1);
-        f.add(tc2);
+       f.add(c1);
+        f.add(c2);
         f.add(l1);
         f.add(l2);
         f.add(l3);
@@ -65,8 +65,8 @@ public void windowClosing(WindowEvent e) {
 public void actionPerformed(ActionEvent e){ 
 try{
 String s1, s2;
-            s1 = (String) tc1.getSelectedItem();
-            s2 = (String) tc2.getSelectedItem();
+            s1 = (String) c1.getSelectedItem();
+            s2 = (String) c2.getSelectedItem();
             if (e.getSource() == b) {
                 if (s1== "Celsius" && s2 == "Celsius") {
                     t2.setText(""+ t1.getText());
